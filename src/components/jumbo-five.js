4bc7d592cd2816e0bfe12d7e6dba172fc7d39fde
09x09 <teemengtan@gmail.com>
2020-07-16 17:22:39 +0800

    added cards

--- src/components/jumbo-five.js
+++ src/components/jumbo-five.js
@@ -1,15 +1,60 @@
 import React from "react"
 
-import { Jumbotron, Container } from "react-bootstrap"
+import { 
+    Jumbotron, 
+    Container,
+    Card,
+    Col,
+    Row
+} from "react-bootstrap"
 
 const JumboFive = () => {
     return (
         <Jumbotron fluid className="jumbo-other">
             <Container className="content-center">
                 <h2>System Overview</h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                </p>
+                <Row>
+                <Col>
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <div class="text-center">
+                            <Card.Title>Fall Detection</Card.Title>
+                            <Card.Text>
+                                Preserving privacy using radar technology and machine learning
+                            </Card.Text>
+                            </div>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                            <Card.Body>
+                                <div class="text-center">
+                                <Card.Title>Activity Levels</Card.Title>
+                                <Card.Text>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                </Card.Text>
+                                </div>
+                        </Card.Body>
+                    </Card>
+                </Col>
+
+                <Col>
+                <Card>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <div class="text-center">
+                            <Card.Title>Networking</Card.Title>
+                            <Card.Text>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            </Card.Text>
+                            </div>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                </Row>
             </Container>
         </Jumbotron>
     )
