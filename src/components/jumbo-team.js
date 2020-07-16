d0e920cd3b0fc4d287a6d637a38df590b631ad1b
lyqht <estee.tsw@gmail.com>
2020-07-16 16:51:05 +0800

    uploaded team photos

--- src/components/jumbo-team.js
+++ src/components/jumbo-team.js
@@ -1,53 +1,46 @@
 import React from "react"
-import face1 from "../images/gatsby-astronaut.png"
+import XavierMeow from "../images/team_members/Xavier.jpg"
+import TeeMengMeow from "../images/team_members/TM.jpg"
+import YiChengMeow from "../images/team_members/yc.jpg"
+import TeckMeow from "../images/team_members/Teck.jpg"
+import KayJinMeow from "../images/team_members/KJ.jpg"
+import JundeMeow from "../images/team_members/JD.jpg"
+import SiewWenMeow from "../images/team_members/sw.png"
 
-import {
-     Jumbotron, 
-     Container, 
-     Col, 
-     Row,
-     Figure
-} from "react-bootstrap"
+import { Jumbotron, Container, Col, Row, Figure } from "react-bootstrap"
 
 const JumboTeam = () => {
-    const MakeFace = (imgsrc, name) => {
-        return (
-            <div class="text-center">
-                <Figure>
-                    <Figure.Image
-                        src= {imgsrc}
-                        roundedCircle
-                        width={200}
-                        height={200}
-                    />
-                    <Figure.Caption className="text-center">
-                        {name}
-                    </Figure.Caption>
-                </Figure>
-            </div>
-        )
-    }
-
+  const MakeFace = (imgsrc, name) => {
     return (
-        <Jumbotron fluid className="jumbo-team">
-            <Container className="content-center">
-                <h2>Meet the Team</h2>
-                <h3> EPD</h3>
-                <Row>
-                    <Col>{MakeFace(face1, "name1")}</Col>                    
-                    <Col>{MakeFace(face1, "name2")}</Col>                    
-                    <Col>{MakeFace(face1, "name3")}</Col>
-                    <Col>{MakeFace(face1, "name4")}</Col>
-                </Row>
-                <h3>ISTD</h3>
-                <Row>
-                    <Col>{MakeFace(face1, "name5")}</Col>
-                    <Col>{MakeFace(face1, "name6")}</Col>
-                    <Col>{MakeFace(face1, "name7")}</Col>
-                </Row>
-            </Container>
-        </Jumbotron>
+      <div class="text-center">
+        <Figure>
+          <Figure.Image src={imgsrc} roundedCircle width={200} height={200} />
+          <Figure.Caption className="text-center">{name}</Figure.Caption>
+        </Figure>
+      </div>
     )
+  }
+
+  return (
+    <Jumbotron fluid className="jumbo-team">
+      <Container className="content-center">
+        <h2>Meet the Team</h2>
+        <h3> EPD</h3>
+        <Row>
+          <Col>{MakeFace(XavierMeow, "Xavier")}</Col>
+          <Col>{MakeFace(TeeMengMeow, "Tee Meng")}</Col>
+          <Col>{MakeFace(KayJinMeow, "Kay Jin")}</Col>
+          <Col>{MakeFace(TeckMeow, "Tiang Teck")}</Col>
+        </Row>
+        <h3>ISTD</h3>
+        <Row>
+          <Col>{MakeFace(JundeMeow, "Jun De")}</Col>
+          <Col>{MakeFace(YiChengMeow, "Yi Cheng")}</Col>
+          <Col>{MakeFace(SiewWenMeow, "Siew Wen")}</Col>
+        </Row>
+      </Container>
+    </Jumbotron>
+  )
 }
 
 export default JumboTeam
