20503c4bcb230cfc0d183e13b293787970262b67
lyqht <estee.tsw@gmail.com>
2020-07-17 01:04:17 +0800

    update mqtt package to different one

--- src/pages/demo.js
+++ src/pages/demo.js
@@ -7,42 +7,10 @@ import "../styles/app.css"
 import Header from "../components/header"
 import { Col, Container, Row } from "react-bootstrap"
 
-var inLivingroom = true
+var inLivingroom = false
 var inBedroom = false
 
-// Setup MQTT
-var client = new Paho.MQTT.Client(
-  location.hostname,
-  Number(location.port),
-  "clientId"
-)
-
-// set callback handlers
-client.onConnectionLost = onConnectionLost
-client.onMessageArrived = onMessageArrived
-
-// connect the client
-client.connect({ onSuccess: onConnect })
-
-// called when the client connects
-function onConnect() {
-  // Once a connection has been made, make a subscription and send a message.
-  console.log("onConnect")
-  client.subscribe("bps/kjhaus/livingroom")
-  client.subscribe("bps/kjhaus/bedroom")
-}
-
-// called when the client loses its connection
-function onConnectionLost(responseObject) {
-  if (responseObject.errorCode !== 0) {
-    console.log("onConnectionLost:" + responseObject.errorMessage)
-  }
-}
-
-// called when a message arrives
-function onMessageArrived(message) {
-  console.log("onMessageArrived:" + message.payloadString)
-}
+var mqtt = require("mqtt")
 
 const DemoPage = () => {
   return (
