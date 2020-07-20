cb3ce7f7b21549d803b4441ec3c2fd79ef4b20ee
lyqht <estee.tsw@gmail.com>
2020-07-20 23:50:24 +0800

    Update demo.js

--- src/pages/demo.js
+++ src/pages/demo.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
-import { Image, Jumbotron } from "react-bootstrap"
+import { Jumbotron, Table } from "react-bootstrap"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "../styles/floorplan.css"
 import "../styles/app.css"
@@ -8,9 +8,32 @@ import Header from "../components/header"
 import { Col, Container, Row } from "react-bootstrap"
 
 var inLivingroom = false
-var inBedroom = false
+var inBedroom = true
 
 var mqtt = require("mqtt")
+var client = mqtt.connect("ws://broker.mqttdashboard.com:1883") //TODO: having trouble with this connection
+
+var topic = "bps/kjhaus/livingroom"
+
+client.on("connect", function () {
+  client.subscribe(topic, function (err) {
+    if (!err) {
+      console.log("Connected to client!")
+    }
+  })
+})
+
+client.on("message", function (topic, message) {
+  if (topic == "fall") {
+    // TODO: add a text to show on screen that a fall is detected at specific timing
+  } else if (topic.split("/")[-1] == "livingroom") {
+    inBedroom = false
+    inLivingroom = true
+  } else if (topic.split("/")[-1] == "bedroom") {
+    inBedroom = true
+    inLivingroom = false
+  }
+})
 
 const DemoPage = () => {
   return (
