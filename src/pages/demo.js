6d2bbaf46619d74e92db294c89e345ed4c4be051
lyqht <estee.tsw@gmail.com>
2020-07-28 01:02:11 +0800

    simplify topic subscription code and update text shown appropriately

--- src/pages/demo.js
+++ src/pages/demo.js
@@ -2,54 +2,48 @@ import React, { useState } from "react"
 import { Container, Jumbotron, Row } from "react-bootstrap"
 import "../styles/floorplan.css"
 
-var inLivingroom = true
-var inBedroom = false
-
-const topic = "bps/kjhaus/livingroom"
-const topic2 = "bps/kjhaus/bedroom"
+var topic1 = "bps/kjhaus/livingroom"
+var topic2 = "bps/kjhaus/bedroom"
+var topics = [topic1, topic2]
 var mqtt = require("mqtt")
 var client = mqtt.connect(process.env.MQTT_URL)
-client.on("connect", function () {
-  client.subscribe(topic, { qos: 1 }, function (err) {
-    if (!err) {
-      console.log("Connected to client on topic!")
-    }
-  })
-  client.subscribe(topic2, { qos: 1 }, function (err) {
-    if (!err) {
-      console.log("Connected to client on topic2!")
-    }
-  })
-})
 
 function DemoApp() {
-  const [mesg, setMesg] = useState(
-    <div>
-      <em>Connected to MQTT Network!</em>
-    </div>
-  )
-
+  const [mesg, setMesg] = useState("Connecting...")
   const [room, setRoom] = useState(0)
+
+  client.on("connect", () => {
+    var subscribed = true
+    for (let x in topics) {
+      client.subscribe(x, { qos: 1 }, err => {
+        if (!err) {
+          console.log("Subscribed to topic!")
+        } else {
+          subscribed = false
+        }
+      })
+    }
+    if (subscribed) {
+      setMesg(
+        "Live demo successfully subscribed to all topics! Waiting for messages..."
+      )
+    }
+  })
 
   client.on("message", function (topic, message) {
     var note = message.toString()
     setMesg(topic + ":" + note)
     if (topic == "fall") {
-      // TODO: add a text to show on screen that a fall is detected at specific timing
+      setMesg("Fall Detected!")
     } else {
-      console.log(topic)
       var splitTopic = topic.split("/")
-      console.log(splitTopic)
-      if (splitTopic[splitTopic.length - 1] === "livingroom") {
-        inBedroom = false
-        inLivingroom = true
+      if (splitTopic[splitTopic.length - 1] === "livingroom" && note === "1") {
         setRoom("in_livingroom")
-        console.log("you are in living room")
-      } else if (splitTopic[splitTopic.length - 1] === "bedroom") {
-        inBedroom = true
-        inLivingroom = false
+      } else if (
+        splitTopic[splitTopic.length - 1] === "bedroom" &&
+        note === "1"
+      ) {
         setRoom("in_bedroom")
-        console.log("you are in bed room")
       }
     }
   })
