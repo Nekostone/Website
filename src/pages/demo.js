dd13ae975192641b4827200b0c7cc208321da3b5
lyqht <estee.tsw@gmail.com>
2020-08-06 22:43:11 +0800

    adding alerts and subsections for diff topics

--- src/pages/demo.js
+++ src/pages/demo.js
@@ -1,16 +1,22 @@
 import React, { useState } from "react"
-import { Container, Jumbotron, Row } from "react-bootstrap"
+import { Alert, Container, Jumbotron, Row } from "react-bootstrap"
 import "../styles/floorplan.css"
+var mqtt = require("mqtt")
 
 var topic1 = "bps/kjhaus/livingroom"
 var topic2 = "bps/kjhaus/bedroom"
-var topics = [topic1, topic2]
-var mqtt = require("mqtt")
+var topic3 = "bps/kjhaus/toilet"
+var topic4 = "bps/kjhaus/exit"
+var topic5 = "fall"
+
+var topics = [topic1, topic2, topic3, topic4, topic5]
 var client = mqtt.connect(process.env.MQTT_URL)
 console.log(process.env.MQTT_URL)
 function DemoApp() {
   const [mesg, setMesg] = useState("Connecting...")
-  const [room, setRoom] = useState(0)
+  const [fallMsg, setFallMsg] = useState("No Falls Detected.")
+  const [fallDetected, setFallDetected] = useState(false)
+  const [room, setRoom] = useState("in_livingroom")
 
   client.on("connect", () => {
     var subscribed = true
@@ -25,32 +31,29 @@ function DemoApp() {
     }
     if (subscribed) {
       setMesg(
-        "Live demo successfully subscribed to all topics! Waiting for messages..."
+        "Live demo successfully subscribed to all topics! Waiting for room status messages..."
       )
     }
   })
 
   client.on("message", function (topic, message) {
     var note = message.toString()
-    setMesg(topic + ":" + note)
-    if (topic == "fall") {
-      setMesg("Fall Detected!")
+
+    if (topic == "fall" && note === "1") {
+      setFallDetected(true)
+      setFallMsg("Fall Detected! " + new Date())
     } else {
       var splitTopic = topic.split("/")
-      if (splitTopic[splitTopic.length - 1] === "livingroom" && note === "1") {
+      var roomIdentified = splitTopic[splitTopic.length - 1]
+      setMesg("User in: " + roomIdentified)
+      if (roomIdentified === "livingroom" && note === "1") {
         setRoom("in_livingroom")
-      } else if (
-        splitTopic[splitTopic.length - 1] === "bedroom" &&
-        note === "1"
-      ) {
+      } else if (roomIdentified === "bedroom" && note === "1") {
         setRoom("in_bedroom")
-      } else if (
-        splitTopic[splitTopic.length - 1] === "toilet" &&
-        note === "1"
-      ) {
+      } else if (roomIdentified === "toilet" && note === "1") {
         setRoom("in_toilet")
-      } else if (splitTopic[splitTopic.length - 1] === "exit" && note === "1") {
-        setRoom("in_exit")
+      } else if (roomIdentified === "exit" && note === "1") {
+        setRoom("not_in_house")
       }
     }
   })
@@ -64,10 +67,25 @@ function DemoApp() {
           </Row>
           <Row>
             <div className="floorplan">
-              <div className={room}></div>
+              <div className={room}>
+                <p>{room}</p>
+              </div>
             </div>
-            <div>
-              <p>{mesg}</p>
+
+            <div className="log">
+              <h4>Room Activity</h4>
+              <Alert key={"status"} variant="primary">
+                {mesg}
+              </Alert>
+
+              <h4>Falls Detected</h4>
+              <Alert
+                className={"fall_detected_" + fallDetected}
+                key={"fall" + new Date().getDate()}
+                variant="danger"
+              >
+                {fallMsg}
+              </Alert>
             </div>
           </Row>
         </Container>
